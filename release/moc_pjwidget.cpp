ef400d9b07c93fbc684d58f1ed4f66bfea384a0c
meoho <46700473+chemica1@users.noreply.github.com>
2019-07-09 10:50:55 +0900

    .

--- release/moc_pjwidget.cpp
+++ release/moc_pjwidget.cpp
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_pjWidget_t {
-    QByteArrayData data[20];
-    char stringdata0[318];
+    QByteArrayData data[14];
+    char stringdata0[195];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -37,28 +37,19 @@ QT_MOC_LITERAL(2, 24, 0), // ""
 QT_MOC_LITERAL(3, 25, 17), // "sigPjDisconnected"
 QT_MOC_LITERAL(4, 43, 11), // "pjConnected"
 QT_MOC_LITERAL(5, 55, 14), // "pjDisconnected"
-QT_MOC_LITERAL(6, 70, 15), // "on_mute_clicked"
-QT_MOC_LITERAL(7, 86, 17), // "on_unmute_clicked"
-QT_MOC_LITERAL(8, 104, 13), // "on_on_clicked"
-QT_MOC_LITERAL(9, 118, 14), // "on_off_clicked"
-QT_MOC_LITERAL(10, 133, 20), // "on_patternOn_clicked"
-QT_MOC_LITERAL(11, 154, 21), // "on_patternOff_clicked"
-QT_MOC_LITERAL(12, 176, 22), // "on_patternNext_clicked"
-QT_MOC_LITERAL(13, 199, 22), // "on_patternPrev_clicked"
-QT_MOC_LITERAL(14, 222, 17), // "updateLampStatus1"
-QT_MOC_LITERAL(15, 240, 17), // "updateLampStatus2"
-QT_MOC_LITERAL(16, 258, 17), // "updateLampStatus3"
-QT_MOC_LITERAL(17, 276, 17), // "updateLampStatus4"
-QT_MOC_LITERAL(18, 294, 9), // "reconnect"
-QT_MOC_LITERAL(19, 304, 13) // "reconnectSlot"
+QT_MOC_LITERAL(6, 70, 13), // "on_on_clicked"
+QT_MOC_LITERAL(7, 84, 14), // "on_off_clicked"
+QT_MOC_LITERAL(8, 99, 17), // "updateLampStatus1"
+QT_MOC_LITERAL(9, 117, 17), // "updateLampStatus2"
+QT_MOC_LITERAL(10, 135, 17), // "updateLampStatus3"
+QT_MOC_LITERAL(11, 153, 17), // "updateLampStatus4"
+QT_MOC_LITERAL(12, 171, 9), // "reconnect"
+QT_MOC_LITERAL(13, 181, 13) // "reconnectSlot"
 
     },
     "pjWidget\0sigPjConnected\0\0sigPjDisconnected\0"
-    "pjConnected\0pjDisconnected\0on_mute_clicked\0"
-    "on_unmute_clicked\0on_on_clicked\0"
-    "on_off_clicked\0on_patternOn_clicked\0"
-    "on_patternOff_clicked\0on_patternNext_clicked\0"
-    "on_patternPrev_clicked\0updateLampStatus1\0"
+    "pjConnected\0pjDisconnected\0on_on_clicked\0"
+    "on_off_clicked\0updateLampStatus1\0"
     "updateLampStatus2\0updateLampStatus3\0"
     "updateLampStatus4\0reconnect\0reconnectSlot"
 };
@@ -70,7 +61,7 @@ static const uint qt_meta_data_pjWidget[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-      18,   14, // methods
+      12,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -78,38 +69,26 @@ static const uint qt_meta_data_pjWidget[] = {
        2,       // signalCount
 
  // signals: name, argc, parameters, tag, flags
-       1,    0,  104,    2, 0x06 /* Public */,
-       3,    0,  105,    2, 0x06 /* Public */,
+       1,    0,   74,    2, 0x06 /* Public */,
+       3,    0,   75,    2, 0x06 /* Public */,
 
  // slots: name, argc, parameters, tag, flags
-       4,    0,  106,    2, 0x0a /* Public */,
-       5,    0,  107,    2, 0x0a /* Public */,
-       6,    0,  108,    2, 0x08 /* Private */,
-       7,    0,  109,    2, 0x08 /* Private */,
-       8,    0,  110,    2, 0x08 /* Private */,
-       9,    0,  111,    2, 0x08 /* Private */,
-      10,    0,  112,    2, 0x08 /* Private */,
-      11,    0,  113,    2, 0x08 /* Private */,
-      12,    0,  114,    2, 0x08 /* Private */,
-      13,    0,  115,    2, 0x08 /* Private */,
-      14,    0,  116,    2, 0x08 /* Private */,
-      15,    0,  117,    2, 0x08 /* Private */,
-      16,    0,  118,    2, 0x08 /* Private */,
-      17,    0,  119,    2, 0x08 /* Private */,
-      18,    0,  120,    2, 0x08 /* Private */,
-      19,    0,  121,    2, 0x08 /* Private */,
+       4,    0,   76,    2, 0x0a /* Public */,
+       5,    0,   77,    2, 0x0a /* Public */,
+       6,    0,   78,    2, 0x08 /* Private */,
+       7,    0,   79,    2, 0x08 /* Private */,
+       8,    0,   80,    2, 0x08 /* Private */,
+       9,    0,   81,    2, 0x08 /* Private */,
+      10,    0,   82,    2, 0x08 /* Private */,
+      11,    0,   83,    2, 0x08 /* Private */,
+      12,    0,   84,    2, 0x08 /* Private */,
+      13,    0,   85,    2, 0x08 /* Private */,
 
  // signals: parameters
     QMetaType::Void,
     QMetaType::Void,
 
  // slots: parameters
-    QMetaType::Void,
-    QMetaType::Void,
-    QMetaType::Void,
-    QMetaType::Void,
-    QMetaType::Void,
-    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -134,20 +113,14 @@ void pjWidget::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, vo
         case 1: _t->sigPjDisconnected(); break;
         case 2: _t->pjConnected(); break;
         case 3: _t->pjDisconnected(); break;
-        case 4: _t->on_mute_clicked(); break;
-        case 5: _t->on_unmute_clicked(); break;
-        case 6: _t->on_on_clicked(); break;
-        case 7: _t->on_off_clicked(); break;
-        case 8: _t->on_patternOn_clicked(); break;
-        case 9: _t->on_patternOff_clicked(); break;
-        case 10: _t->on_patternNext_clicked(); break;
-        case 11: _t->on_patternPrev_clicked(); break;
-        case 12: _t->updateLampStatus1(); break;
-        case 13: _t->updateLampStatus2(); break;
-        case 14: _t->updateLampStatus3(); break;
-        case 15: _t->updateLampStatus4(); break;
-        case 16: _t->reconnect(); break;
-        case 17: _t->reconnectSlot(); break;
+        case 4: _t->on_on_clicked(); break;
+        case 5: _t->on_off_clicked(); break;
+        case 6: _t->updateLampStatus1(); break;
+        case 7: _t->updateLampStatus2(); break;
+        case 8: _t->updateLampStatus3(); break;
+        case 9: _t->updateLampStatus4(); break;
+        case 10: _t->reconnect(); break;
+        case 11: _t->reconnectSlot(); break;
         default: ;
         }
     } else if (_c == QMetaObject::IndexOfMethod) {
@@ -199,13 +172,13 @@ int pjWidget::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 18)
+        if (_id < 12)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 18;
+        _id -= 12;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 18)
+        if (_id < 12)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 18;
+        _id -= 12;
     }
     return _id;
 }
